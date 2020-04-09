120c1708a354e781c8795fcb704feabef2bc9a94
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-04-09 00:35:04 -0300

    End of project

--- http/api/index.js
+++ http/api/index.js
@@ -1,38 +1,40 @@
-const http = require('http');
-const data = require('./urls.json');
-const URL = require('url');
-const fs = require('fs');
-const path = require('path');
+const http = require('http')
+const URL = require('url')
+const fs = require('fs')
+const path = require('path')
 
-function writeFile(cb){
+const data = require('./urls.json')
+
+function writeFile(cb) {
     fs.writeFile(
         path.join(__dirname, "urls.json"), 
         JSON.stringify(data, null, 2),
         err => {
-            if(err) throw err;
+            if(err) throw err
 
-            cb(JSON.stringify({message: "ok"}));
+            cb(JSON.stringify({message: "ok"}))
         }
-     );
+    )
 }
 
 http.createServer((req, res) => {
-    const { name, url, del } = URL.parse(req.url, true).query;
-  
+    const { name, url, del } = URL.parse(req.url, true).query
+
     res.writeHead(200, {
-        'Acess-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': '*'
     })
 
-    if(!name || !url){
-        return res.end(JSON.stringify(data));
-    }
-    if(del){
-       data.urls = data.urls.filter(item => String(item.url) != String(url));
-       
-       return writeFile(message => res.end(message));
+    // all resources
+    if(!name || !url)
+        return res.end(JSON.stringify(data))
+
+    if(del) {
+        data.urls = data.urls.filter(item => String(item.url) !== String(url))
+        return writeFile((message) => res.end(message))   
     }
 
-    data.urls.push({name, url});
-    writeFile(message => res.end(message));
+    data.urls.push({name, url})
 
-}).listen(3000, () => console.log('API Rodando...'));
+    return writeFile((message) => res.end(message)) 
+
+}).listen(3000, () => console.log('Api is running'))
--- http/public/scripts.js
+++ http/public/scripts.js
@@ -3,26 +3,50 @@ const input = document.querySelector("input")
 const form = document.querySelector('form')
 
 
+async function load() {
+    const res = await fetch("http://localhost:3000/").then((data) => data.json())
+
+    res.urls.map(({name, url}) => addElement({name, url}))
+}
+async function create({name, url}){
+    const archiveCreate = `name=${name}&url=${url}`;
+    const res = await fetch(`http://localhost:3000/?${archiveCreate}`).then((message) => message.json());
+    
+}
+async function deleteLink({name, url, del}){
+    const archiveCreate = `name=${name}&url=${url}&del=${del}`;
+    const res = await fetch(`http://localhost:3000/?${archiveCreate}`).then((message) => message.json());
+}
+
 function addElement({ name, url }) {
     const li = document.createElement('li')
     const a = document.createElement("a")
     const trash = document.createElement("span")
+    const obj = {
+        "name" : name,
+        "url" : url,
+        "del" : 1,
+    }
 
     a.href = url
     a.innerHTML = name
     a.target = "_blank"
 
     trash.innerHTML = "x"
-    trash.onclick = () => removeElement(trash)
+    trash.onclick = () => removeElement(trash, obj);
 
     li.append(a)
     li.append(trash)
     ul.append(li)
+
+    create({ name, url });
 }
 
-function removeElement(el) {
-    if (confirm('Tem certeza que deseja deletar?'))
-        el.parentNode.remove()
+function removeElement(el, obj) {
+    if (confirm('Tem certeza que deseja deletar?')){
+        el.parentNode.remove();
+        deleteLink(obj);
+    }    
 }
 
 form.addEventListener("submit", (event) => {
@@ -41,7 +65,10 @@ form.addEventListener("submit", (event) => {
     if (!/^http/.test(url)) 
         return alert("Digite a url da maneira correta")
 
-    addElement({ name, url })
+    addElement({ name, url });
 
     input.value = ""
+
+
 })
+load();
