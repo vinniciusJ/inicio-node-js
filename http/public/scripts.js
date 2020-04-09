f78da40b0f191c0fc1e947da6549c7ab727bff61
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-04-09 11:15:02 -0300

    Add ultimas funcoes

--- http/public/scripts.js
+++ http/public/scripts.js
@@ -1,45 +1,82 @@
 const ul = document.querySelector("ul")
 const input = document.querySelector("input")
 const form = document.querySelector('form')
-
+var init = 0;
 
 async function load() {
+    init++;
+
     const res = await fetch("http://localhost:3000/").then((data) => data.json())
 
-    res.urls.map(({name, url}) => addElement({name, url}))
+    res.urls.map(({name, url}) => loadElement({name, url}))
 }
 async function create({name, url}){
     const archiveCreate = `name=${name}&url=${url}`;
     const res = await fetch(`http://localhost:3000/?${archiveCreate}`).then((message) => message.json());
-    
+
 }
 async function deleteLink({name, url, del}){
     const archiveCreate = `name=${name}&url=${url}&del=${del}`;
     const res = await fetch(`http://localhost:3000/?${archiveCreate}`).then((message) => message.json());
 }
-
-function addElement({ name, url }) {
-    const li = document.createElement('li')
-    const a = document.createElement("a")
-    const trash = document.createElement("span")
+async function check(urlReq){
+    const res = await fetch("http://localhost:3000/").then((data) => data.json());
+    var cond = true;
+    await res.urls.map(({url}) => {
+        if(url == urlReq){
+            cond = fase;
+        }
+    })
+    return cond;
+}
+async function loadElement({name, url}){
+    const li = document.createElement('li');
+    const a = document.createElement("a");
+    const trash = document.createElement("span");
     const obj = {
         "name" : name,
         "url" : url,
         "del" : 1,
-    }
+    };
+    a.href = url;
+    a.innerHTML = name;
+    a.target = "_blank";
 
-    a.href = url
-    a.innerHTML = name
-    a.target = "_blank"
+    trash.innerHTML = "x";
+    trash.onclick = () => removeElement(trash, obj);
+    
+    li.append(a);
+    li.append(trash);
+    ul.append(li);
+}
+async function addElement({ name, url }) {
+    const li = document.createElement('li');
+    const a = document.createElement("a");
+    const trash = document.createElement("span");
+    const obj = {
+        "name" : name,
+        "url" : url,
+        "del" : 1,
+    };
 
-    trash.innerHTML = "x"
+    a.href = url;
+    a.innerHTML = name;
+    a.target = "_blank";
+
+    trash.innerHTML = "x";
     trash.onclick = () => removeElement(trash, obj);
 
-    li.append(a)
-    li.append(trash)
-    ul.append(li)
-
-    create({ name, url });
+    if(await check(obj['url'])){
+        li.append(a);
+        li.append(trash);
+        ul.append(li);
+        create({ name, url });
+    }
+    else {
+        alert('Esse link já existe');
+        return;
+    }
+    
 }
 
 function removeElement(el, obj) {
@@ -52,22 +89,23 @@ function removeElement(el, obj) {
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let { value } = input
+    let { value } = input;
 
-    if (!value) 
+    if (!value) {
         return alert('Preencha o campo')
+    }
 
-    const [name, url] = value.split(",")
+    const [name, url] = value.split(",");
 
-    if (!url) 
+    if (!url) {
         return alert('formate o texto da maneira correta')
-
-    if (!/^http/.test(url)) 
+    }
+    if (!/^http/.test(url)) {
         return alert("Digite a url da maneira correta")
-
+    }
     addElement({ name, url });
 
-    input.value = ""
+    input.value = "";
 
 
 })
